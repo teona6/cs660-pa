843412c445276c136a0620096b628e52aef66f17
bagashvilit <bagashvilit@allegheny.edu>
2023-10-28 15:04:03 -0400

    minor fix

--- objects/db/CMakeFiles/db.dir/TransactionId.cpp.o.d
+++
@@ -1,4 +0,0 @@
-db/CMakeFiles/db.dir/TransactionId.cpp.o: \
- /home/teona/Documents/courses/CS660/cs660-pa/db/TransactionId.cpp \
- /usr/include/stdc-predef.h \
- /home/teona/Documents/courses/CS660/cs660-pa/db/../include/db/TransactionId.h
--- objects/examples/CMakeFiles/iterator.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for iterator.
-# This may be replaced when dependencies are built.
